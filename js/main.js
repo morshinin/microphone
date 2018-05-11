52ec073eb21babf57c57eabc1078d003d9932513
Mikhail <314159265absurdus@gmail.com>
2018-05-11 08:47:52 +0300

    Styled slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
   $('.slider').slick({
-    
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.arrow-prev'),
+    nextArrow: $('.arrow-next'),
+    responsive: [
+      {
+        breakpoint: 1439,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
