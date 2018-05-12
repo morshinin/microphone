0e4da7da68fa849d6141a2df6f80a982ac3ea7ae
Mikhail <314159265absurdus@gmail.com>
2018-05-12 06:59:25 +0300

    Finishes desktop sections gallery and gift

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	// Testimonial slider init
   $('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -31,3 +32,24 @@ $(document).ready(function(){
   // Slicknav
 	$('.pageNav').slicknav();
 });
+// Gift inputs
+const giftRadio = document.querySelectorAll('input[name=gift]'),
+			pbWrapper = document.querySelector('.giftImageWrapper_pb'),
+			hphWrapper = document.querySelector('.giftImageWrapper_hph')
+
+giftRadio.forEach(radio => {
+	radio.addEventListener('change', e => {
+		switch (e.target.id) {
+			case 'powerBank':
+					pbWrapper.classList.add('giftImage_pb_cord_active')
+					hphWrapper.classList.remove('giftImage_hph_cord_active')
+				break
+			case 'headphones':
+			pbWrapper.classList.remove('giftImage_pb_cord_active')
+			hphWrapper.classList.add('giftImage_hph_cord_active')
+				break
+			default:
+				break
+		}
+	})
+})
